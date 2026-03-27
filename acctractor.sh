d20de33aff43d383e5759aaa111ffdb344f04f50
afra818 <60651664+afra818@users.noreply.github.com>
2026-03-27 09:58:50 +0000

    f

--- acctractor.sh
+++ acctractor.sh
@@ -10,7 +10,7 @@
 module load anaconda3/2022.05 
 source activate square_att
 cd /scratch/hpc/07/zhang303/square_att
-srun python clip_cifar10_anchor_defense_stratified.py \
+srun python clip_cifar10_anchor_defense.py \
     --data_root ./data \
     --batch_size 64 \
     --samples_per_class 100 \
